540471b65e8ddafcd0e1ac29fa03f959ab94c3bb
ryedigaryan <ruben.yedigaryan@gmail.com>
2018-10-12 16:28:23 +0400

    moving backend to server/ folder

--- server/src/main/java/com/vmware/ncg/controllers/IndexController.java
+++ server/src/main/java/com/vmware/ncg/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package com.vmware.ncg.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping("/")
     public String index() {
         return "index";
